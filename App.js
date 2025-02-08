3bc83a84b6b882b0b1d3eb229ab98b0fab365df1
jyotideshmukh <jyotipawar2003@gmail.com>
2025-02-08 23:40:40 +0530

    createElement -second param {} used for adding attributes

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
 //how to create element in react 
-const heading = React.createElement("h1",{},'Hello World from React');
+//in createElement-- first param is tag
+//second param is object for giving multiple attributes, 
+// currently I have given "id" and "className" attributes
+//third param is for content of element
+const heading = React.createElement(
+    "h1",
+    {
+        "id":"heading",
+        "className":"green-h1"
+    },
+    'Hello World from React');
 //create root in react
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //rendering heading inside root
