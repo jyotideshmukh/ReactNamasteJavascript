dbd4d057105e4da6509f086c33f416684f40658f
jyotideshmukh <jyotipawar2003@gmail.com>
2025-02-09 11:09:28 +0530

    hierarchical html created using react

--- App.js
+++ App.js
@@ -3,13 +3,18 @@
 //second param is object for giving multiple attributes, 
 // currently I have given "id" and "className" attributes
 //third param is for content of element
-const heading = React.createElement(
-    "h1",
-    {
-        "id":"heading",
-        "className":"green-h1"
-    },
-    'Hello World from React');
+const heading = React.createElement("div",
+    { id: "parent" },
+    React.createElement("div",
+        { id: "child" },
+        React.createElement(
+            "h1",
+            {
+                "id": "heading",
+                "className": "green-h1"
+            },
+            'Hello World from React')));
+console.log(heading);
 //create root in react
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //rendering heading inside root
