12261c48f727303bac940d3c52504a4d7d1f8495
jyotideshmukh <jyotipawar2003@gmail.com>
2025-02-20 21:50:01 +0530

    state in class

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,16 @@
-const User = (props)=>{
-    const {name, address, location} = props;
+import { useState } from "react";
+
+const User = (props) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
+    const { name, address, location } = props;
     return (
         <ul>
-        <li>Name: {name}</li>
-        <li>Address: {address}</li>
-        <li>Location: {location}</li>
+            <li>Count: {count}</li>
+            <li>Count2: {count2}</li>
+            <li>Name: {name}</li>
+            <li>Address: {address}</li>
+            <li>Location: {location}</li>
         </ul>
     )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,19 +1,28 @@
 import React from "react";
-class UserClass extends React.Component {
-    constructer(props) {
-        Super(props)
 
+class UserClass extends React.Component {
+    constructor(props) {
+        super(props); // Fixed typo here
+    
+        this.state = {
+            count: 0,
+            count2: 1
+        };
     }
 
     render() {
         const { name, address, location } = this.props;
+        const { count, count2 } = this.state;
         return (
             <ul className="user-class">
+                <li>Count: {count}</li>
+                <li>Count2: {count2}</li>
                 <li>Name: {name}</li>
                 <li>Address: {address}</li>
                 <li>Location: {location}</li>
             </ul>
-        )
+        );
     }
 }
+
 export default UserClass;
