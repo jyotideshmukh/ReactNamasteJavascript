4728e8e7416e718c21c702b125b2050f79fa0fe2
jyotideshmukh <jyotipawar2003@gmail.com>
2025-02-23 17:16:36 +0530

    git user profile api used in class component in componentDidMount method

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,28 @@
 import User from "./User";
 import UserClass from "./UserClass";
+import React from "react";
 
-const About = () =>{
+class About extends React.Component
+{
+    constructor(props){
+        super(props)
+        console.log("Parent constructor called")
+    }
+    componentDidMount(){
+        console.log("Parent componentDidMount called ")
+       
+    }
+    render(){
+        console.log("Parent render called")
     return (
+       
         <>
         <div className="app-container">
         <h1>About us</h1>
-        <div> <User name={"Jyoti Deshmukh"} address={"Pune"} location={"Baner"}/></div>
         <div> <UserClass name={"Jyoti Deshmukh"} address={"Pune"} location={"Baner"}/></div>
         </div>
             </>
     )
+}
 }
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,32 +4,45 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props); // Fixed typo here
     
-        this.state = {
+        /*this.state = {
             count: 0,
             count2: 1
         };
+        console.log("child constuctor called")*/
+        this.state ={
+            userInfo:{
+                name:"User's name",
+                location: "User Location",
+                
+            }
+        }
+    }
+    async componentDidMount(){
+        //console.log("Child componentDidMount called ")
+        //console.log( "componentDidMount mainly used for API calling")
+        const data = await fetch("https://api.github.com/users/jyotideshmukh")
+        const json = await data.json();
+        this.setState({
+            userInfo: {
+                name: json.name,
+                location: json.location,
+                avatar_url: json.avatar_url,
+            }
+        })
+        console.log(this.state.userInfo)
     }
 
     render() {
-        const { name, address, location } = this.props;
-        const { count, count2 } = this.state;
+        const { name, location, avatar_url } = this.state.userInfo;
+        console.log("child render called");
         return (
             
           
                 <div className="user-class">
-                <button onClick={()=>{
-                    //how to modify the state variables
-                    this.setState({
-                        count:this.state.count+1
-                })
-                    console.log(count);
-                }}>Increase Count</button>
-                <ul>
-                <li>Count: {count}</li>
-                <li>Name: {name}</li>
-                <li>Address: {address}</li>
-                <li>Location: {location}</li>
-                </ul>
+               <img src={avatar_url}/>
+                <h3>Name: {name}</h3>
+                <h3>Location: {location}</h3>
+        
                 </div>
             
             
